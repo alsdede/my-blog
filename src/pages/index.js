4c73272371d362667feafc761359614f95b4d9df
Andre <als.andresilveira@gmail.com>
2020-09-17 11:20:32 -0300

    Add markdown data to index page and connect with graphql

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,56 @@
 import React from "react"
+import { useStaticQuery } from 'gatsby'
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
 import PostItem from '../components/PostItem'
 
-const IndexPage = () => (
-  <Layout>
+const IndexPage = () => {
+  const { allMarkdownRemark } = useStaticQuery(graphql`
+    query PostList {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title,
+              date(locale:"pt-br",formatString: "DD [de] MMM [de] YYYY")
+              description,
+              category,
+              background
+            }
+            timeToRead
+          }
+        }
+      }
+    }
+  `)
+
+  const postList = allMarkdownRemark.edges
+  return(
+
+    <Layout>
     <SEO title="Home" />   
-    <PostItem
-      slug="/about/"
-      category="Misc"
-      date="30 de Julho de 2019"
-      timeToRead="5"
-      title="Diga não ao Medium: tenha sua própria plataforma"
-      description="Algumas razões para você ter sua própria plataforma ao invés de soluções como o Medium."
-    />
+    {postList.map(({
+      node:{
+        frontmatter:{ background, category, date, description, title},
+        timeToRead
+        }})=>(
+          <PostItem
+          slug="/about/"
+          background={background}
+          category={category}
+          date={date}
+          timeToRead={timeToRead}
+          title={title}
+          description={description}
+          />
+        ))}
+   
   </Layout>
-)
+  )
+
+
+}
+ 
+
 
 export default IndexPage
